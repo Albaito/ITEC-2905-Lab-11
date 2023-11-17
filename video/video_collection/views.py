cb669c67254d71aa2b772f902aeb47f1e0073fe5
Albaito <stanfordtramaine44@gmail.com>
2023-11-17 05:16:48 -0600

    up to slide 36

--- video/video_collection/views.py
+++ video/video_collection/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Video
+from .forms import VideoForm
 
-# Create your views here.
+def home(request):
+    app_name = 'Video List'
+    return render(request, 'video_collection/home.html', {'app_name': app_name})
+
+def add(request):
+    new_video_form = VideoForm()
+    return render(request, 'video_collection/add.html', {'new_video_form' : new_video_form})
