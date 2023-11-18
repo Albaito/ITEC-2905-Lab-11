3c0c9cbbf707ff1f2c86d81a8d77d684d76bd7b6
Albaito <stanfordtramaine44@gmail.com>
2023-11-18 05:04:56 -0600

    up to slide 50

--- video/video_collection/models.py
+++ video/video_collection/models.py
@@ -4,6 +4,7 @@ class Video(models.Model):
     name = models.TextField(max_length=200)
     url = models.TextField(max_length=400)
     notes = models.TextField(blank=True, null=True)
+    video_id = models.CharField(max_length=40, unique=True, default='None')
 
     def __str__(self):
         
--- video/video_collection/views.py
+++ video/video_collection/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 from .models import Video
 from .forms import VideoForm
+from django.contrib import messages
 
 def home(request):
     app_name = 'Video List'
     return render(request, 'video_collection/home.html', {'app_name': app_name})
 
 def add(request):
+    if request.method == 'POST':
+        new_video_form = VideoForm(request.POST)
+        if new_video_form.is_valid():
+            new_video_form.save()
+            messages.info(request, 'New video saved!')
+            # TODO: redirect to a list of videos
+        else:
+            messages.warning(request, 'Check the data entered!')
+            return render(request, 'video_collection/add.html', {'new_video_form' : new_video_form})
+        
     new_video_form = VideoForm()
-    return render(request, 'video_collection/add.html', {'new_video_form' : new_video_form})
+    return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
+
+def video_list(request):
+    videos = Video.objects.get.order_by('name')
+    return render(request, 'video_collection/video_list.html', {'videos': videos})
